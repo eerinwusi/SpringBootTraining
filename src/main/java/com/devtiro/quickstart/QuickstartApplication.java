7943239aea1bd36b424942a076386cec9f9bdde4
eerinwusi <emmanuel.erinwusi@payaza.africa>
2023-12-02 00:44:19 -0800

    Connecting to a H2 in-memory database

--- src/main/java/com/devtiro/quickstart/QuickstartApplication.java
+++ src/main/java/com/devtiro/quickstart/QuickstartApplication.java
@@ -1,13 +1,31 @@
 package com.devtiro.quickstart;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class QuickstartApplication {
+@Log
+public class QuickstartApplication implements CommandLineRunner {
+
+	private final DataSource dataSource;
+
+	public QuickstartApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickstartApplication.class, args);
 	}
 
+	@Override
+	public void run(final String... args) throws Exception {
+		log.info("Datasource " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
 }
